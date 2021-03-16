05d6061cf753a7a8bb55e9ac48ecb687032121b0
PRASHANT-2018-KUSHWAHA <prashantkushwaha000@gmail.com>
2021-03-17 01:00:40 +0530

    changes

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
-app.use( express.static(__dirname + '/client' ) );
+app.use( express.static(__dirname + '/client/dist/amit-demo' ) );
 
-var listener = server.listen(process.env.PORT || 5000, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+var listener = server.listen(process.env.PORT || 4200, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 4200
 });
+
